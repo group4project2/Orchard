3ec30c5f7971bec84563a1d26496079a08ce00fd
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-07-22 19:26:46 -0500

    newFiles

---
+++ Config/schema.js
@@ -0,0 +1,12 @@
+// main table column titles:
+// ID number
+// name
+// quanty (qty)
+// price ($)
+// delivery date (mm/dd received)
+// expiration date (mm/dd)
+// on sale? (T/F)
+
+
+// 7:24
+// also: category (fruit, vegetable, plant, or flower)
