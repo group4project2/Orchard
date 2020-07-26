c022445700c45d1cee76bb9eea7f5b610e2b2c4f
Steve Munoz <stevomunoz@yahoo.com>
2020-07-26 16:16:30 -0500

    created a react app and a Login component and rendered page successfully

---
+++ orchard/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import LoginPage from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
