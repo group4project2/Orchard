435bc51e012b50e7d833661e297d5d4ddab120fd
Steve Munoz <stevomunoz@yahoo.com>
2020-08-01 11:36:07 -0500

    created association for Inventory and Items

--- models/categories.js
+++ models/categories.js
@@ -1,18 +1,15 @@
 module.exports = function (sequelize, DataTypes) {
-  var Category = sequelize.define(
-    'Category',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  var Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
     },
-    {},
-  );
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
   return Category;
 };
--- models/employees.js
+++ models/employees.js
@@ -1,31 +1,34 @@
 module.exports = function (sequelize, DataTypes) {
-  var Employee = sequelize.define(
-    'Employee',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-
-      last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  var Employee = sequelize.define('Employee', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {},
-  );
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
   return Employee;
 };
