01d7451c5e732e5144132de472ad431c8969947d
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-08-02 15:30:42 -0500

    cleaned up display so it matches the wireframes

---
+++ client/src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Navbar(props) {
+    return (
+        <nav className="navbar fixed-top">
+        <img src="../orchard.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+        <h4 id="logo">orchard</h4>
+        </nav>
+    );
+}
+export default Navbar;
