b8e71f39a2c25582230245e2bef9408231e440d3
Steve Munoz <stevomunoz@yahoo.com>
2020-08-07 16:02:56 -0500

    commented out allownull: false

--- models/Item.js
+++ models/Item.js
@@ -1,33 +1,30 @@
 const Sequelize = require('sequelize');
 const db = require('../database');
 
-module.exports = db.sequelize.define(
-  'item',
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    quantity: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-    price: {
-      type: Sequelize.DECIMAL,
-      allowNull: false,
-    },
-    date_received: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-    exp_date: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    }
-  }
-);
+module.exports = db.sequelize.define('item', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    // allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    // allowNull: false,
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    // allowNull: false,
+  },
+  date_received: {
+    type: Sequelize.DATE,
+    // allowNull: false,
+  },
+  exp_date: {
+    type: Sequelize.DATE,
+    // allowNull: false,
+  },
+});
