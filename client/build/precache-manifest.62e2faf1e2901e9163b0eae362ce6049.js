de689c5645f85f77265b358ed00cb81a4e3329e6
Steve Munoz <stevomunoz@yahoo.com>
2020-08-09 12:39:21 -0500

    removed build from .gitignore

---
+++ client/build/precache-manifest.62e2faf1e2901e9163b0eae362ce6049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbaaee64faeeeea971db093db258f73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1d879703562dc5f5dc9",
+    "url": "/static/css/main.bc68481b.chunk.css"
+  },
+  {
+    "revision": "d33ae648b12d3ff683ad",
+    "url": "/static/js/2.b82c0a53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b82c0a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d879703562dc5f5dc9",
+    "url": "/static/js/main.c572b91e.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  },
+  {
+    "revision": "0d56e78e82dff7471f10a8a3b334fbb6",
+    "url": "/static/media/bgd.0d56e78e.png"
+  }
+]);
