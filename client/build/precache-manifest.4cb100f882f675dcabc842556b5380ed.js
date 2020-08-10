6e534f31e5b5764e30136883c5ace9253a4ea985
Steve Munoz <stevomunoz@yahoo.com>
2020-08-09 22:20:50 -0500

    ran npm run build

--- client/build/precache-manifest.4cb100f882f675dcabc842556b5380ed.js
+++ client/build/precache-manifest.4cb100f882f675dcabc842556b5380ed.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7eaf12f3e8704e2ef4ed9112db6c1402",
+    "revision": "0150dce20b18ea79821c3304c1e66f6c",
     "url": "/index.html"
   },
   {
-    "revision": "53e0751f99f78fcd5b34",
+    "revision": "6025775261c434097fd4",
     "url": "/static/css/main.beb59aac.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0c9c1dfd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "53e0751f99f78fcd5b34",
-    "url": "/static/js/main.ff7ee49c.chunk.js"
+    "revision": "6025775261c434097fd4",
+    "url": "/static/js/main.88859fe9.chunk.js"
   },
   {
     "revision": "712a493cb6bbaa95b925",
