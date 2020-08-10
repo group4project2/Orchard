fcf02ce21e2f7508f37f86b5ea4daf2109634952
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-08-10 17:41:18 -0500

    julietcard

---
+++ client/build/precache-manifest.f555eca5dfcba3e89d033e8ee2b07302.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfaf84de4b24c9e31c204c89594c0a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae163e121a406d0991a9",
+    "url": "/static/css/main.bc68481b.chunk.css"
+  },
+  {
+    "revision": "7bc80e3a92228296401c",
+    "url": "/static/js/2.14f8699b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.14f8699b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae163e121a406d0991a9",
+    "url": "/static/js/main.8274d372.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  },
+  {
+    "revision": "0d56e78e82dff7471f10a8a3b334fbb6",
+    "url": "/static/media/bgd.0d56e78e.png"
+  }
+]);
